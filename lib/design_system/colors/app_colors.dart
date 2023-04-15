86b17fa66a9492d49c764be347e07a19b409e929
Luan <marcosluan68@gmail.com>
2023-04-15 11:40:16 -0400

    feat(design_system): create AppColors class

---
+++ lib/design_system/colors/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color darkBlue = Color(0xFF023262);
+  static const Color blue = Color(0xFF1366B4);
+  static const Color lightBlue = Color(0xFFF3FFFF);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color blueGrey = Colors.blueGrey;
+  static const Color lightGrey = Color(0xFFDFE9F2);
+  static const Color grey = Color(0xFF979FA9);
+  static const Color black = Colors.black;
+}
