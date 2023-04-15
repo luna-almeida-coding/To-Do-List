913531390a4381447a887e316c5412827d8421f0
Luan <marcosluan68@gmail.com>
2023-04-15 19:29:12 -0400

    test(datasource): write unit test for getTodoList method in ToDo datasource

--- lib/features/to_do_list/data/datasources/to_do_datasouce.dart
+++ lib/features/to_do_list/data/datasources/to_do_datasouce.dart
@@ -1,5 +1,22 @@
+import 'package:to_do_list_squad_premiun/core/local_storage/local_storage.dart';
 import 'package:to_do_list_squad_premiun/features/to_do_list/data/models/to_do_model.dart';
 
 abstract class IToDoDatasource {
   Future<List<ToDoModel>> getToDoList();
+}
+
+class ToDoDatasource implements IToDoDatasource {
+  final LocalStorage localStorage;
+
+  ToDoDatasource(this.localStorage);
+
+  @override
+  Future<List<ToDoModel>> getToDoList() async {
+    final result = await localStorage.read(key: 'ToDoList');
+    List<ToDoModel> toDoModelList = [];
+
+    toDoModelList.addAll(result);
+
+    return toDoModelList;
+  }
 }
---
+++ test/features/to_do_list/data/datasources/to_do_datasource_test.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:to_do_list_squad_premiun/core/local_storage/local_storage.dart';
+import 'package:to_do_list_squad_premiun/features/to_do_list/data/datasources/to_do_datasouce.dart';
+
+import '../../../../mocks/to_do_mocks.dart';
+
+class MockLocalStore extends Mock implements LocalStorage {}
+
+void main() {
+  late ToDoDatasource datasource;
+  late LocalStorage localStorage;
+
+  setUp(
+    () {
+      localStorage = MockLocalStore();
+      datasource = ToDoDatasource(localStorage);
+    },
+  );
+
+  test(
+    'Should return a List of ToDoModel from the Local Storage',
+    () async {
+      final Map<String, Object> values = <String, Object>{
+        'ToDoList': mockTodoListModel,
+      };
+
+      SharedPreferences.setMockInitialValues(values);
+      when(() => localStorage.read(key: 'ToDoList')).thenAnswer(
+        (_) async => mockTodoListModel,
+      );
+
+      final result = await datasource.getToDoList();
+
+      expect(result, mockTodoListModel);
+    },
+  );
+}
