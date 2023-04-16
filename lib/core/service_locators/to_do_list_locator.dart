1e360ba0c7dbc9a0891302557eac5a0ace03b892
Luan <marcosluan68@gmail.com>
2023-04-16 19:17:45 -0400

    feat(DI): set DI for ToDoCubit

--- lib/core/service_locators/to_do_list_locator.dart
+++ lib/core/service_locators/to_do_list_locator.dart
@@ -1,12 +1,18 @@
 import 'package:to_do_list_squad_premiun/core/local_storage/local_storage.dart';
+import 'package:to_do_list_squad_premiun/core/local_storage/shared_preferences_implementation.dart';
 import 'package:to_do_list_squad_premiun/core/service_locators/service_locator.dart';
 import 'package:to_do_list_squad_premiun/features/to_do_list/data/datasources/to_do_datasource.dart';
 import 'package:to_do_list_squad_premiun/features/to_do_list/data/repositories/to_do_repository_implementation.dart';
 import 'package:to_do_list_squad_premiun/features/to_do_list/domain/repositories/i_to_do_repository.dart';
 import 'package:to_do_list_squad_premiun/features/to_do_list/domain/use_cases/get_to_do_list_use_case.dart';
 import 'package:to_do_list_squad_premiun/features/to_do_list/domain/use_cases/update_to_do_list_use_case_use_case.dart';
+import 'package:to_do_list_squad_premiun/features/to_do_list/presentation/cubits/to_do_list_cubit.dart';
 
 Future<void> setUpToDoListLocators() async {
+  getIt.registerFactory<LocalStorage>(
+    () => SharedPreferencesImplementation(),
+  );
+
   getIt.registerFactory<IToDoDatasource>(
     () => ToDoDatasource(
       getIt<LocalStorage>(),
@@ -28,6 +34,13 @@ Future<void> setUpToDoListLocators() async {
   getIt.registerFactory<UpdateToDoListUsecase>(
     () => UpdateToDoListUsecase(
       getIt<IToDoRepository>(),
+    ),
+  );
+
+  getIt.registerFactory<ToDoListCubit>(
+    () => ToDoListCubit(
+      toDoListUseCase: getIt<GetToDoListUseCase>(),
+      updateToDoListUseCase: getIt<UpdateToDoListUsecase>(),
     ),
   );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:to_do_list_squad_premiun/core/service_locators/service_locator.dart';
+import 'package:to_do_list_squad_premiun/features/to_do_list/presentation/cubits/to_do_list_cubit.dart';
 import 'package:to_do_list_squad_premiun/features/to_do_list/presentation/pages/home_page.dart';
 
 void main() async {
@@ -13,9 +15,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'To Do List',
-      home: HomePage(),
+      home: BlocProvider(
+        create: (BuildContext context) => getIt<ToDoListCubit>(),
+        child: HomePage(),
+      ),
     );
   }
 }
