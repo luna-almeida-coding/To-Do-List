e44037e75ed54892947e97816b58ecc6c209b261
Luan <marcosluan68@gmail.com>
2023-04-16 19:19:02 -0400

    fix(ToDoDatasource): adjust to/from json implementation

--- lib/features/to_do_list/data/datasources/to_do_datasource.dart
+++ lib/features/to_do_list/data/datasources/to_do_datasource.dart
@@ -1,4 +1,5 @@
 import 'package:to_do_list_squad_premiun/core/constants/constants.dart';
+import 'package:to_do_list_squad_premiun/core/errors/exceptions/exceptions.dart';
 import 'package:to_do_list_squad_premiun/core/local_storage/local_storage.dart';
 import 'package:to_do_list_squad_premiun/features/to_do_list/data/models/to_do_model.dart';
 import 'package:to_do_list_squad_premiun/features/to_do_list/domain/entities/to_do_entity.dart';
@@ -17,24 +18,34 @@ class ToDoDatasource implements IToDoDatasource {
   @override
   Future<List<ToDoModel>> getToDoList() async {
     final result = await localStorage.read(key: defaultToDoListKey);
-    List<ToDoModel> toDoModelList = [];
 
-    toDoModelList.addAll(result);
+    if (result == null) throw GenericException();
+
+    List<ToDoModel> toDoModelList = [];
+    List<String> toDoStringList = result.map<String>((e) => e.toString()).toList();
+
+    toDoModelList = toDoStringList.map((e) => ToDoModel.fromJson(e)).toList();
 
     return toDoModelList;
   }
 
   @override
   Future<bool> updateToDoList(List<ToDoEntity> list) async {
-    List<ToDoModel> modelList = [];
+    List<String> modelList = [];
 
     for (var i in list) {
-      modelList.add(ToDoModel.fromEntity(i));
+      modelList.add(
+        ToDoModel.fromEntity(i).toJson().toString(),
+      );
     }
 
-    return await localStorage.write(
+    final result = await localStorage.write(
       key: defaultToDoListKey,
       data: modelList,
     );
+
+    if (!result) throw GenericException();
+
+    return result;
   }
 }
