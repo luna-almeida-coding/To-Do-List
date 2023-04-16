d29e62d11449336ebeafa5086b25b135b8094f51
Luan <marcosluan68@gmail.com>
2023-04-16 19:18:10 -0400

    feat(ToDoModel): add to/from json implementation

--- lib/features/to_do_list/data/models/to_do_model.dart
+++ lib/features/to_do_list/data/models/to_do_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:to_do_list_squad_premiun/features/to_do_list/domain/entities/to_do_entity.dart';
 
 class ToDoModel extends ToDoEntity {
@@ -12,4 +14,22 @@ class ToDoModel extends ToDoEntity {
       isCompleted: entity.isCompleted,
     );
   }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'description': description,
+      'isCompleted': isCompleted,
+    };
+  }
+
+  factory ToDoModel.fromMap(Map<String, dynamic> map) {
+    return ToDoModel(
+      description: map['description'],
+      isCompleted: map['isCompleted'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory ToDoModel.fromJson(String data) => ToDoModel.fromMap(json.decode(data));
 }
